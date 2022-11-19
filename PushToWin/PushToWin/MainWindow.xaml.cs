3ad4279421703c2e719e9b1446a5ac0193e2b3f4
garobcsi <gasparrobert0908@gmail.com>
2022-11-19 14:45:28 +0100

    StartWindow done (WireFrame)

--- PushToWin/PushToWin/MainWindow.xaml.cs
+++ PushToWin/PushToWin/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PushToWin.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,32 @@ namespace PushToWin
     /// </summary>
     public partial class MainWindow : Window
     {
+        StartWindowModel context = new StartWindowModel();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            DataContext = context;
+        }
+
+        private void Label_MouseEnter(object sender, MouseEventArgs e)
+        {
+            context.ImgSwitch((sender as Label).Name.ToString());
+        }
+
+        private void Label_MouseLeave(object sender, MouseEventArgs e)
+        {
+            context.ImgSwitch((sender as Label).Name.ToString());
+        }
+
+        private void Label_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            //Test
+            btStart.Content = (sender as Label).Name.ToString();
+            //Test end
         }
     }
 }
---
+++ PushToWin/PushToWin/ViewModels/StartWindowModel.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace PushToWin.ViewModels
+{
+    public class StartWindowModel : INotifyPropertyChanged
+    {
+        #region ImgChangeModel
+        private string
+            start = "Start.png",
+            levelSelector = "LevelSelector.png",
+            mapEditor = "MapEditor.png",
+            quit = "Quit.png",
+            //HoverImg
+            startHover = "Logo.png",
+            levelHover = "Logo.png",
+            mapEditorHover = "Logo.png",
+            quitHover = "Logo.png";
+        public string Start
+        {
+            get
+            {
+                return "/img/mainwindow/" + start;
+            }
+            set
+            {
+                start = value;
+                PropertyChangedHandler(nameof(start));
+            }
+        }
+        public string LevelSelector
+        {
+            get
+            {
+                return "/img/mainwindow/" + levelSelector;
+            }
+            set
+            {
+                levelSelector = value;
+                PropertyChangedHandler(nameof(levelSelector));
+            }
+        }
+        public string MapEditor
+        {
+            get
+            {
+                return "/img/mainwindow/" + mapEditor;
+            }
+            set
+            {
+                mapEditor = value;
+                PropertyChangedHandler(nameof(mapEditor));
+            }
+        }
+        public string Quit {
+            get
+            {
+                return "/img/mainwindow/" + quit;
+            }
+            set
+            {
+                quit = value;
+                PropertyChangedHandler(nameof(quit));
+            }
+        }
+        public void ImgSwitch(string name) {
+            switch (name)
+            {
+                case "btStart":
+                    (start, startHover) = (startHover, start);
+                    PropertyChangedHandler(nameof(start));
+                    break;
+                case "btLevelSelector":
+                    (levelSelector, levelHover) = ( levelHover, levelSelector);
+                    PropertyChangedHandler(nameof(levelSelector));
+                    break;
+                case "btMapEditor":
+                    (mapEditor, mapEditorHover) = (mapEditorHover, mapEditor);
+                    PropertyChangedHandler(nameof(mapEditor));
+                    break;
+                case "btQuit":
+                    (quit, quitHover) = (quitHover,quit);
+                    PropertyChangedHandler(nameof(quit));
+                    break;
+            }
+        }
+        #endregion
+        public event PropertyChangedEventHandler PropertyChanged;
+        void PropertyChangedHandler([CallerMemberName] string property = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
