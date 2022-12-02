5003bbe0efc77b7dd1c551cd88fda3dd9f97edca
BalazsBenceLevente <balazs.bence300@gmail.com>
2022-12-02 14:06:01 +0100

    osztályok
    A játék két alap objektuma amiket a játékos használni fog.

---
+++ PushToWin/PushToWin/Class/Kő.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PushToWin.Class
+{
+    class Kő : Objects
+    {
+        Objects fix = new Objects(); 
+
+    }
+}
